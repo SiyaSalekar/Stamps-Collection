38504a8a5abab70f2af527e27fcdb8729105b344
SiyaSalekar <92158161+SiyaSalekar@users.noreply.github.com>
2022-03-01 12:45:00 +0000

    added functionality to edit stamps

--- includes/header.php
+++ includes/header.php
@@ -1,14 +1,17 @@
 <!-- the head section -->
 <head>
 <title>Stamp It</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Shizuru&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
 
 <link rel="stylesheet" type="text/css" href="css/mainstyle.css">
 </head>
 
 <!-- the body section -->
 <body>
-<header><h1>Stamp It</h1></header>
+<header><h1>Stamp It <span class="material-icons">&#xe982;</span></h1></header>
