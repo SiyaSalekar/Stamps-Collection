d735cf13668f997dde845bb15abce07554f1c27f
SiyaSalekar <92158161+SiyaSalekar@users.noreply.github.com>
2022-03-17 17:22:31 +0000

    Updated Readme file

--- README.md
+++ README.md
@@ -1,8 +1,38 @@
 # Stamp It
-![alt text](image_uploads/log.png "Logo Title Text 1")
-* A starter project for DkIT students on the Server-Side module.
+![alt text](image_uploads/log.png "Logo")
+⋅⋅⋅Browse through a Fascinating Stamps Collection.
 ### Instructions
 * Create a MySQL database called php-crud in PHP MyAdmin
 * Run the code in the sql-db.txt file in PHP MyAdmin
 * Move the PHP files in to your htdocs folder so Apache can process the PHP code
 * Configure the database.php file to connect to your SQL database
+
+======
+
+# Home Page
+![alt text](image_uploads/home.JPG "Home")
+
+======
+
+# Search for your favorite stamp
+![alt text](image_uploads/search.JPG "Search")
+
+======
+
+# Toggle Sidebar
+![alt text](image_uploads/sidebar.JPG "Sidebar")
+
+======
+
+# Manage Stamp Categories
+![alt text](image_uploads/manage.JPG "Manage")
+
+======
+
+# Easy to use Forms
+![alt text](image_uploads/fom.JPG "Forms")
+
+We encourage you to use our App.
+Thank you
+
+&copy; DkIT 2022 - Siya Salekar
--- category_list.php
+++ category_list.php
@@ -14,38 +14,42 @@
 <?php
 include('includes/header.php');
 ?>
-    <h1>Category List</h1>
+    <h1 class="add-title">Manage Stamp Categories</h1>
     <table>
         <tr>
-            <th>Name</th>
+            
             <th>&nbsp;</th>
         </tr>
         <?php foreach ($categories as $category) : ?>
         <tr>
-            <td><?php echo $category['categoryName']; ?></td>
+        
+            <td>
+            <div class="form-vertical"><?php echo $category['categoryName']; ?><br><br></td>
             <td>
                 <form action="delete_category.php" method="post"
                       id="delete_product_form">
-                    <input type="hidden" name="category_id"
+                    <input class="form-control" type="hidden" name="category_id"
                            value="<?php echo $category['categoryID']; ?>">
-                    <input type="submit" value="Delete">
+                    <input class="btn btn-warning rounded-pill" type="submit" value="Delete">
                 </form>
+                </div>
             </td>
+        
         </tr>
         <?php endforeach; ?>
     </table>
     <br>
 
     <h2>Add Category</h2>
-    <form action="add_category.php" method="post"
+    <form  class="form-vertical" action="add_category.php" method="post"
           id="add_category_form">
 
         <label>Name:</label>
-        <input type="input" name="name">
-        <input id="add_category_button" type="submit" value="Add">
+        <input class="form-control" type="input" name="name">
+        <input class="btn btn-warning rounded-pill" id="add_category_button" type="submit" value="Add">
     </form>
     <br>
-    <p><a href="index.php">Homepage</a></p>
+    <p class="home-btn"><a href="index.php">Homepage</a></p>
 
     <?php
 include('includes/footer.php');
